caa8de989e748b8421fbbb4e968da6641d21b549
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-04 14:25:02 -0700

    Practice delegates

--- Delegates_20241123_task2/Program.cs
+++ Delegates_20241123_task2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Delegates_20241123_task2
+﻿using System.Globalization;
+
+namespace Delegates_20241123_task2
 {
 //    Создайте и заполните массив на 30 элементов.
 //Создайте 4 метода, которые принимают массив и выполняют: 
@@ -25,6 +27,7 @@
     {
         static void Main(string[] args)
         {
+            int[] nums = PopulateArray(30);
             Del del1 = AscSort;
             Del del2 = DescSort;
             Del del3 = EvenSort;
@@ -32,29 +35,150 @@
 
             Del del5 = del1 + del2 + del3 + del4;
 
-            Console.WriteLine("Hello, World!");
+            del5(nums);
+
+            Console.ReadKey();
         }
 
         public delegate int[] Del(int[] nums);
 
-        //public static int[] AscSort(int[] nums)
-        //{
+        public static int[] PopulateArray(int num)
+        {
+            Random rnd = new Random();
+            int[] nums = new int[num];
 
-        //}
+            for (int i = 0; i < num; i++)
+            {
+                nums[i] = rnd.Next(-99, 100);
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nFreshly created array:");
+            Console.ResetColor();
+            PrintArray(nums);
+            return nums;
+        }
+        public static void PrintArray(int[] nums)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == nums.Length - 1)
+                {
+                    Console.Write($" {nums[i]}");
+                }
+                else
+                {
+                    Console.Write($"{nums[i]} |");
+                }               
+            }
+            Console.WriteLine();
+        }
 
-        //public static int[] DescSort(int[] nums)
-        //{
+        public static int[] AscSort(int[] nums)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nAscSort in progress.....");
+            Console.ResetColor();
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            PrintArray(nums);
+            return nums;
+        }
 
-        //}
+        public static int[] DescSort(int[] nums)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nDescSort in progress.....");
+            Console.ResetColor();
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            PrintArray(nums);
+            return nums;
+        }
 
-        //public static int[] EvenSort(int[] nums)
-        //{
+        public static int[] EvenSort(int[] nums)
+        {
+            int count = 0;
 
-        //}
+            for (int i = 0;i < nums.Length;i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
 
-        //public static int[] UnevSort(int[] nums)
-        //{
+            int[] nums2 = new int[count];
 
-        //}
+            int count2 = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    nums2[count2] = nums[i];
+                    count2++;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nEven sort completed:");
+            Console.ResetColor();
+            PrintArray(nums2);
+            return nums2;
+        }
+
+        public static int[] UnevSort(int[] nums)
+        {
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 1)
+                {
+                    count++;
+                }
+            }
+
+            int[] nums2 = new int[count];
+
+            int count2 = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 1)
+                {
+                    nums2[count2] = nums[i];
+                    count2++;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nUNeven sort completed:");
+            Console.ResetColor();
+            PrintArray(nums2);
+            return nums2;
+        }
     }
 }
