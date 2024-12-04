f62d15e6df5a90f15b8ba33057b78afc3cc775a1
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-04 13:47:41 -0700

    Add project files.

---
+++ Delegates_20241123/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegates_20241123
+{
+    //Создать метод выводящий на экран приветствие. Создать делегат. Добавить метод в делегат. Вызвать делегат.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Del del = Hello;
+
+            del.Invoke();
+
+            Console.ReadLine();
+        }
+
+        public delegate void Del();
+        public static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
