7c0f6e77e7fc37787161f9c9568455c14255e5c7
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-04 15:11:51 -0700

    Practice delegates

---
+++ 20241204_SendMessageDelegatesTask2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _20241204_SendMessageDelegatesTask2
+{
+    internal class Program
+    {
+        //Создайте класс «SendMessage». Опишите на уровне класса делегат,
+        //переменную делегата и метод для отправки сообщения SendMessage с 2-мя параметрами: Заголовок и текст сообщения. 
+        //Используя переменную делегата, дать возможность пользователю прописать свою логику отправки сообщения в методе SendMessage.
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+
+            message.del1("fancy title", "blah blah blah");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Message
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public delegate void Del(string title, string text);
+
+        public Del del1 = SendMessage;
+
+        public static void SendMessage(string title, string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Title:\n{title}");
+            Console.ResetColor();
+            Console.WriteLine($"Text:\n{text}");
+        }
+    }
+}
